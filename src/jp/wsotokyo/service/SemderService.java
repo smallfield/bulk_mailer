6cfb145b4608bf2873151111c13c68843ec0688e
Tomoharu Ono <rio.carbon@gmail.com>
2011-05-18 23:31:00 +0900

    add sender modification
    Change-Id: I341801b770c242f1fb8e8970c82c9b84823b4f32

---
+++ src/jp/wsotokyo/service/SemderService.java
@@ -0,0 +1,16 @@
+package jp.wsotokyo.service;
+
+import java.util.List;
+
+import jp.wsotokyo.meta.SenderMeta;
+import jp.wsotokyo.model.Sender;
+
+import org.slim3.datastore.Datastore;
+
+public class SemderService {
+    static SenderMeta m = new SenderMeta();
+
+    public static List<Sender> getList() {
+        return Datastore.query(m).asList();
+    }
+}
